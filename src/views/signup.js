b892d029a1709c5f85fdee596b3a1ff2a565760f
Andrei Geacar <andrei.geacar@gmail.com>
2016-11-09 22:43:47 +0200

    writing user type to firebase db

--- src/views/signup.js
+++ src/views/signup.js
@@ -1,12 +1,12 @@
 'use strict';
 import  {
-  AppRegistry,
-  Text,
-  TextInput,
-  View,
-  Picker,
+    AppRegistry,
+    Text,
+    TextInput,
+    View,
+    Picker,
 } from 'react-native';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 import Button from '../components/button';
 import Header from '../components/header';
@@ -15,114 +15,127 @@ import Login from './login';
 
 import styles from '../styles/common-styles.js';
 
-import app from '../config/config.js' 
+import app from '../config/config.js'
 
 export default class signup extends Component {
 
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      loaded: true,
-      email: '',
-      password: ''
-    };
-  }
+        this.state = {
+            loaded: true,
+            email: '',
+            password: '',
+            type: 'asd'
+        };
 
-  signup(){
+    }
 
-    var self = this;
-    this.setState({
-      loaded: false
-    });
+    escapeEmailAddress(email) {
+        if (!email) return false;
+        email = email.toLowerCase();
+        email = email.replace(/\./g, ',');
+        return email;
+    }
 
-    app.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(
-	
-	function (result) {
-		   alert('Your account was created!');
-		   self.setState({
-				email: '',
-				password: '',
-				loaded: true
-			  });
-	  }
-	  ,
-	
-    function(error) {
-        switch(error.code){
-
-          case "EMAIL_TAKEN":
-            alert("The new user account cannot be created because the email is already in use.");
-          break;
-
-          case "INVALID_EMAIL":
-            alert("The specified email is not a valid email.");
-          break;
-
-          default:
-            alert("Error creating user:");
-        }
-		self.setState({
-				email: '',
-				password: '',
-				loaded: true
-			  });
-
-      });
-	  
-	 
+    completeSignup() {
+        var userRef = app.database().ref('autistica/users').child(this.escapeEmailAddress(this.state.email));
+        userRef.set({email: this.state.email, type: this.state.type});
+    }
 
 
-  }
+    signup() {
 
-  goToLogin(){
-    this.props.navigator.push({
-      component: Login
-    });
-  }
+        var self = this;
+        this.setState({
+            loaded: false
+        });
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header text="Signup" loaded={this.state.loaded} />
-        <View style={styles.body}>
+        app.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then(
+            function (result) {
+                self.completeSignup();
+                alert('Your account was created!');
+                self.setState({
+                    email: '',
+                    password: '',
+                    loaded: true
+                });
+            }
+            ,
 
-            <TextInput
-                style={styles.textinput}
-                onChangeText={(text) => this.setState({email: text})}
-                value={this.state.email}
-            placeholder={"Email Address"}
-            />
-          <TextInput
-            style={styles.textinput}
-            onChangeText={(text) => this.setState({password: text})}
-            value={this.state.password}
-            secureTextEntry={true}
-            placeholder={"Password"}
-          />
-            <Picker
-                style={styles.picker}
-                selectedValue={this.state.type}
-                onValueChange={(value) => this.setState({"type":value})}
-            >
-                <Picker.Item label="Need Help" value="asd" />
-                <Picker.Item label="Offer Help"  value="caretaker" />
-            </Picker>
-          <Button
-            text="Register"
-            onpress={this.signup.bind(this)}
-            button_styles={styles.primary_button}
-            button_text_styles={styles.primary_button_text} />
+            function (error) {
+                switch (error.code) {
 
-          <Button
-            text="Already have an account?"
-            onpress={this.goToLogin.bind(this)}
-            button_styles={styles.transparent_button}
-            button_text_styles={styles.transparent_button_text} />
-        </View>
-      </View>
-    );
-  }
+                    case "EMAIL_TAKEN":
+                        alert("The new user account cannot be created because the email is already in use.");
+                        break;
+
+                    case "INVALID_EMAIL":
+                        alert("The specified email is not a valid email.");
+                        break;
+
+                    default:
+                        alert("Error creating user:");
+                }
+                self.setState({
+                    email: '',
+                    password: '',
+                    loaded: true
+                });
+
+            });
+
+
+    }
+
+    goToLogin() {
+        this.props.navigator.push({
+            component: Login
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Header text="Signup" loaded={this.state.loaded}/>
+                <View style={styles.body}>
+
+                    <TextInput
+                        style={styles.textinput}
+                        onChangeText={(text) => this.setState({email: text})}
+                        value={this.state.email}
+                        placeholder={"Email Address"}
+                    />
+                    <TextInput
+                        style={styles.textinput}
+                        onChangeText={(text) => this.setState({password: text})}
+                        value={this.state.password}
+                        secureTextEntry={true}
+                        placeholder={"Password"}
+                    />
+                    <Picker
+                        style={styles.picker}
+                        selectedValue={this.state.type}
+                        onValueChange={(value) => this.setState({"type": value})}
+                    >
+                        <Picker.Item label="Need Help" value="asd"/>
+                        <Picker.Item label="Offer Help" value="caretaker"/>
+                    </Picker>
+                    <Button
+                        text="Register"
+                        onpress={this.signup.bind(this)}
+                        button_styles={styles.primary_button}
+                        button_text_styles={styles.primary_button_text}/>
+
+                    <Button
+                        text="Already have an account?"
+                        onpress={this.goToLogin.bind(this)}
+                        button_styles={styles.transparent_button}
+                        button_text_styles={styles.transparent_button_text}/>
+                </View>
+            </View>
+        );
+    }
 }
 
 AppRegistry.registerComponent('signup', () => signup);
